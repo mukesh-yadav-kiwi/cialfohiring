007a65e3c0c648c0ee9e4d69d565ff227acb7664
Mukesh <ashu.3333d@gmail.com>
2016-09-14 15:25:55 +0530

    second commit.

--- are_anagrams.rb
+++ are_anagrams.rb
@@ -1,10 +1,19 @@
 module AreAnagrams
     def self.are_anagrams?(string_a, string_b)
         if string_a && string_b
-          if string_a.sum == string_b.sum
-            return true
-          else
+          if string_a.length != string_b.length
             return false
+          else
+            char_hash = Hash.new(0)
+            string_a.each_char do |element|
+              char_hash[element] += 1
+            end
+            string_b.each_char do |element|
+              return false  if char_hash[element] <= 0
+              char_hash[element] -= 1
+            end
+
+            return true
           end
         else
           return "Not a valid input"
